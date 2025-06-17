9b81509182391ef6fa827e7c4647de1b23b387e6
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2025-06-18 00:29:29 +0900

    update all

--- src/components/layout/shared-component.tsx
+++ src/components/layout/shared-component.tsx
@@ -490,7 +490,7 @@ margin-top: 10px;
 `;
 
 export const ZakekeDesignerContainer = styled.div<{ $isMobile?: boolean }>`
-    /* height: 75%; */
+    height: 75%;
     max-width: 50%;
     min-width: 50%;
     width: 50%;
